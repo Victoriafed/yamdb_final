289bd17b16ee6136843267a02a500c9626267c98
Victoriafed <victoriafedorova2018@mail.ru>
2023-05-05 17:46:16 +0300

    v2.4.2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -64,9 +64,7 @@ class GenreViewSet(CustomMixinSet):
 class CategoryViewSet(CustomMixinSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsAdminModeratorOwnerOrReadOnly,
-                          )
+    permission_classes = (AdminSuperUserPermission,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
